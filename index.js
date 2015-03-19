f5ad44eb7dd8ee7843bba3559c25d6e69b8ca9f8
ronxjansen <ronxjansen@gmail.com>
2015-03-19 15:06:34 +0100

    Added basic event handler

---
+++ index.js
@@ -0,0 +1,54 @@
+var _ = require('underscore');
+var async = require('async');
+
+module.exports = (function() {
+  var handlers = {};
+  return {
+
+      add: function(key, fn) {
+
+        if (!_.isString(key))
+          return new Error('First argument should be an string');
+
+        if (!_.isFunction(fn))
+          return new Error('Second argument should be an function');
+
+        var keys = (key.split('.').length > 1)
+          ? key.split('.') : key;
+
+        // TODO: implement fn to dynamicly create nested objects based
+        handlers[keys[0]] = {};
+        handlers[keys[0]][keys[1]] = fn;
+      },
+
+      /**
+       * Async executes all applied event handlers. Verifies if array is
+       * a valid array, plus validates if objects is an object. Other validation
+       * will be placed in handlers itself.
+       * @param  {Array}    arr     [description]
+       * @param  {Object}   options [description]
+       * @param  {Function} cb      [description]
+       * @return {}         err     [description]
+       */
+      exec: function(client, arr, options, cb) {
+        var appliedHandlers = {};
+
+        if (!_.isArray(arr))
+          return cb(new Error('First argument should be an array'));
+
+        if (!_.isObject(options))
+          return cb(new Error('Second argument should be an object'));
+
+        _.each(arr, function(val) {
+          if (val && handlers[val]) {
+            appliedHandlers.push(handlers[val]);
+          }
+        });
+
+        async.each([options], async.applyEach(appliedHandlers), function(err) {
+          if (err) return cb(err);
+          cb(null);
+        });
+      }
+  };
+})();
